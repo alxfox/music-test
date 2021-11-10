edb148fc275565d4d68fcfc68cc41bd014787bea
Alexander Fuchs <fuchs.alx@web.de>
2021-11-10 16:31:21 +0100

    some stuff

--- main.js
+++ main.js
@@ -1,14 +1,30 @@
-const { app, BrowserWindow, Menu } = require('electron');
-const menu = require('./menu');
+const { app, BrowserWindow, Menu, net } = require("electron");
+const menu = require("./menu");
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
     },
-    resizable: false
+    resizable: false,
   });
-  win.loadFile('index.html');
+  win.loadFile("index.html");
 }
-app.on('ready', createWindow);
+function abcd() {
+  const request = net.request(
+    ""
+  );
+  request.on("response", (response) => {
+    response.on("data", (chunk) => {
+      console.log(`BODY: ${chunk}`);
+    });
+    response.on("end", () => {
+      console.log("No more data in response.");
+    });
+  });
+  request.end()
+  console.log("hello there");
+}
+app.on("ready", createWindow);
+app.on("ready", abcd);
